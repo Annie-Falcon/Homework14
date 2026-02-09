fab55d0a8f2d18431b6fb6f0a86ba47d2cc38b7d
Annie-Falcon <anna81283@mail.ru>
2026-02-10 01:29:04 +0300

    Done homework

--- src/org/skypro/skyshop/search/SearchEngine.java
+++ src/org/skypro/skyshop/search/SearchEngine.java
@@ -1,5 +1,7 @@
 package org.skypro.skyshop.search;
 
+import org.skypro.skyshop.exeptions.BestResultNotFound;
+
 import java.util.Arrays;
 
 public class SearchEngine {
@@ -40,4 +42,37 @@ public class SearchEngine {
         System.out.println("Результат поиска: '" + searchString + "'");
         System.out.println(Arrays.toString(listResult));
     }
+
+    public Searchable searchMaxCoincidence(String searchString) throws BestResultNotFound {
+        int indexMax = -1;
+        int countCoincidenceMax = 0;
+        int i = 0;
+        int indexBeg;
+        int indexIn;
+        int countCoincidence;
+
+        for (Searchable searchable : listSearch) {
+            if (searchable != null) {
+                countCoincidence = 0;
+                indexBeg = 0;
+                indexIn = searchable.getSearchTerm().indexOf(searchString, indexBeg);
+                while (indexIn != -1) {
+                    countCoincidence++;
+                    indexBeg = indexIn + searchString.length();
+                    indexIn = searchable.getSearchTerm().indexOf(searchString, indexBeg);
+                }
+                if (countCoincidence > countCoincidenceMax) {
+                    countCoincidenceMax = countCoincidence;
+                    indexMax = i;
+                }
+            }
+            i++;
+        }
+        if (indexMax == -1) {
+            throw new BestResultNotFound(searchString);
+        }
+        return listSearch[indexMax];
+    }
+
+
 }
